6b32c302d91a7e2d0c05a467a2ad6ce29bfc0883
kirkade <mookj@mail.ru>
2022-10-17 10:12:35 +0300

    again debug headers

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Routes, Route, Navigate} from 'react-router-dom'
+import {Routes, Route, Navigate } from 'react-router-dom'
 import Error404 from './pages/Error404'
 import PreJunior from './pages/PreJunior'
 import Junior from './pages/Junior'
@@ -13,15 +13,16 @@ export const PATH = {
 }
 
 function Pages() {
+
     return (
         <div>
             {/*Routes выбирает первый подходящий роут*/}
             <Routes>
-                <Route path={'/*'} element={<Error404/>}/>
-                <Route path={'/'} element={<PreJunior/>}/>
-                <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
-                <Route path={PATH.JUNIOR} element={<Junior/>}/>
-                <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
+                <Route path={'/'} element={<Navigate to={PATH.PRE_JUNIOR}/>}/>
+                <Route path={'*'} element={<Error404/>} />
+                <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>} />
+                <Route path={PATH.JUNIOR} element={<Junior/>} />
+                <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>} />
             </Routes>
         </div>
     )
