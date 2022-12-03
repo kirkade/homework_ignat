2fbfa2623e2a33b7d657e194d35a86a2cb154cf4
kirkade_desktop <mookj@mail.ru>
2022-12-03 20:46:09 +0300

    add logic hw12

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -2,13 +2,27 @@ const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+type stateType = {
+    themeId:number
+}
+
+type changeThemeIdActionType ={
+    type:'SET_THEME_ID',
+    id: number
+}
+
+
+
+export const themeReducer = (state = initState, action: changeThemeIdActionType): stateType => { // fix any
     switch (action.type) {
         // дописать
 
+        case 'SET_THEME_ID' : {
+            return {...state, themeId:action.id}
+        }
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: number): changeThemeIdActionType => ({ type: 'SET_THEME_ID', id } as const) // fix any
